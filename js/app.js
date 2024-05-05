1aa408f325c12ca6eddc993edc7084b49f4e1668
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-05-05 18:27:35 +0500

    init

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items: 1,
+        dots: false,
+        smartSpeed: 1000,
+        navText : ['PREV', 'NEXT'],
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                
+            },
+            600:{
+                
+            },
+            1000:{
+                
+            }
+        }
+    })
+})
